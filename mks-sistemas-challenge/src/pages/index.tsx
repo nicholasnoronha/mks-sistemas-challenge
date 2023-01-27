3625be7b4c36b04ab4e84728695143416ad43d7b
Nicholas Noronha <nicholascaio@hotmail.com>
2023-01-26 21:29:18 -0300

    feat: fetch data from api

--- mks-sistemas-challenge/src/pages/index.tsx
+++ mks-sistemas-challenge/src/pages/index.tsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { Fragment } from "react";
+import axios, { AxiosError } from "axios";
 
-const Home: React.FC = () => {
-  return <div>Home</div>;
+const Home: React.FC = (props: any) => {
+  return (
+    <Fragment>
+      <div>Home</div>
+      {props.products.map((product: any) => {
+        return (
+          <p key={product.id}>
+            {product.id}, {product.name}, {product.description}, {product.photo}
+            , {product.price}, {product.brand}
+          </p>
+        );
+      })}
+    </Fragment>
+  );
 };
+
+export async function getStaticProps() {
+  const url =
+    "https://mks-challenge-api-frontend.herokuapp.com/api/v1/products?page=1&rows=8&sortBy=id&orderBy=ASC";
+
+  const response = await axios.get(url);
+
+  return {
+    props: {
+      products: response.data.products,
+    },
+  };
+}
 
 export default Home;
