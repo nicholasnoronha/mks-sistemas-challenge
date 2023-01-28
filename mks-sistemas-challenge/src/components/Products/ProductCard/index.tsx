bdb525c3d3cf66565ed3cfa293ad530790910108
Nicholas Noronha <nicholascaio@hotmail.com>
2023-01-27 23:06:01 -0300

    feat: create product card component

---
+++ mks-sistemas-challenge/src/components/Products/ProductCard/index.tsx
@@ -0,0 +1,56 @@
+import { Product } from "@/interfaces/Product";
+import { itemsCounterActions } from "@/store";
+import { Icon } from "@iconify/react";
+import React from "react";
+import { useDispatch } from "react-redux";
+import Button from "./Button";
+import CardContainer from "./styles";
+
+interface ProductCardProps {
+  product: Product;
+}
+
+const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
+  const dispatch = useDispatch();
+
+  const incrementHandler = () => {
+    dispatch(itemsCounterActions.increment());
+  };
+
+  const addProductsHandler = () => {
+    dispatch(itemsCounterActions.addProduct(product));
+  };
+
+  return (
+    <div
+      style={{
+        boxShadow: "0px 2px 8px rgba(0, 0, 0, 0.135216)",
+        borderRadius: 8,
+      }}
+    >
+      <CardContainer>
+        <div className="teste">
+          <div className="img-container">
+            <img src={product.photo} />
+          </div>
+          <div className="card-title">
+            <p>{product.name}</p>
+            <p className="product-price">R${product.price}</p>
+          </div>
+          <p className="product-description">{product.description}</p>
+        </div>
+      </CardContainer>
+      <Button
+        onClick={() => {
+          addProductsHandler();
+          incrementHandler();
+        }}
+      >
+        <Icon icon="ri:shopping-bag-3-line" width="17" height="17" />
+        COMPRAR
+      </Button>
+    </div>
+  );
+};
+
+export default ProductCard;
