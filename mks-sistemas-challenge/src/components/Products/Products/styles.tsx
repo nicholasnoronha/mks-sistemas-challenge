4d6589dd9130b16f17793a797195032f85796750
Nicholas Noronha <nicholascaio@hotmail.com>
2023-01-27 23:07:35 -0300

    styles: add styles to products component

---
+++ mks-sistemas-challenge/src/components/Products/Products/styles.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const ProductsContainer = styled.div`
+  display: grid;
+  gap: 2rem;
+  grid-template-columns: 217px 217px 217px 217px;
+  position: relative;
+  place-items: center;
+  height: 100%;
+  padding: 116px 0;
+`;
+
+export default ProductsContainer;
