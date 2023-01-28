804049aa42f36d1d53a090715d60777af9441bd2
Nicholas Noronha <nicholascaio@hotmail.com>
2023-01-27 23:02:04 -0300

    feat: create interfaces

---
+++ mks-sistemas-challenge/src/interfaces/Product.ts
@@ -0,0 +1,11 @@
+export interface Product {
+  id: number;
+  name: string;
+  description: string;
+  brand: string;
+  price: string;
+  photo: string;
+  createdAt: string;
+  updatedAt: string;
+  amount?: number;
+}
--- mks-sistemas-challenge/src/pages/checkout/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Checkout: React.FC = () => {
-  return <div>Checkout</div>;
-};
-
-export default Checkout;
